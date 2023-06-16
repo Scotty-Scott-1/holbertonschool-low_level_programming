f4070786c4665e7807c9127429bfa7733bc6653f
Solomon <swilliamfr@outlook.com>
2023-06-16 10:51:54 -0700

    added file 6

---
+++ hello_world/6-size.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Description: A function that prints a statement
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char chartype;
+int inttype;
+long int longinttype;
+long long int longlonginttype;
+float floattype;
+
+	printf("Size of a char: %ld bytes\n", sizeof(chartype));
+	printf("Size of a int: %ld bytes\n", sizeof(inttype));
+	printf("Size of a long int: %ld bytes\n", sizeof(longinttype));
+	printf("Size of a long long int: %ld bytes\n", sizeof(longlonginttype));
+	printf("Size of a float: %ld bytes\n", sizeof(floattype));
+
+
+
+
+
+
+
+
+
+
+        return (0);
+}
