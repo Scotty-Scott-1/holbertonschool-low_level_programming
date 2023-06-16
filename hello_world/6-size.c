7bb9cd14fa07f576d33f63da1fa1662f5ea0e5ee
Solomon <swilliamfr@outlook.com>
2023-06-16 11:08:54 -0700

    added file 6 x3

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,28 +8,19 @@
  *
  * Return: 0
  */
-int main(void)
+	int main(void)
 {
-char chartype;
-int inttype;
-long int longinttype;
-long long int longlonginttype;
-float floattype;
+	char chartype;
+	int inttype;
+	long int longinttype;
+	long long int longlonginttype;
+	float floattype;
 
 	printf("Size of a char: %ld byte(s)\n", sizeof(chartype));
 	printf("Size of a int: %ld byte(s)\n", sizeof(inttype));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(longinttype));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(longlonginttype));
-	printf("Size of a float: %ld byte(s)\n", sizeof(floattype));
+	printf("Size of a float: %ld byte(s)\n", sizeof(floattype))
 
-
-
-
-
-
-
-
-
-
-        return (0);
+	return (0);
 }
