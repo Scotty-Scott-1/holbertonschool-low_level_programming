19f23fd378deb61912623d468e2a23437a1bd00d
Solomon <swilliamfr@outlook.com>
2023-06-16 09:51:42 -0700

    added file 5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Description: A function that prints a statement 
+ *
+ * Return: 0
+ */
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,n/l");
+
+	return (0);
+}
+
+
