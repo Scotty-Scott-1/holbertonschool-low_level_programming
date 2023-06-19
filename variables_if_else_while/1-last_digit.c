a97240e31bf93bb1a0d87210965e020ae20218a7
Solomon <swilliamfr@outlook.com>
2023-06-19 07:19:53 -0700

    corrected file 1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -7,7 +7,7 @@
 * Description - a function
 *
 * Return: 0
-*/ 
+*/
 int main(void)
 {
 	int n;
@@ -15,19 +15,19 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lastn = n%10;
+	lastn = n % 10;
 	printf("Last digit of %d is ", n);
-	if(n > 5)
+	if (lastn > 5)
 	{
 	printf("%d and is greater than 5", lastn);
 	}
-	else if (n == 0) 
+	else if (lastn == 0)
 	{
 	printf("0 is 0");
 	}
 	else
-	{	
-	printf ("%d and is less than 6 and not 0", lastn);
+	{
+	printf("%d and is less than 6 and not 0", lastn);
 	}
 	return (0);
 }
