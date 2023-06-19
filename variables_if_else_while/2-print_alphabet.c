c2e0bf79b0bbcc1cfefda529bbc43095bb313d89
Solomon <swilliamfr@outlook.com>
2023-06-19 08:10:15 -0700

    added file 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description - a function
+*
+* Return: 0
+*/
+int main(void)
+{
+
+	int a;
+	char b;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+	b = a;
+	putchar(b);
+	}
+	return (0);
+}
