e0b83997e6df156405783216c8e6cfeff714c14b
Solomon <swilliamfr@outlook.com>
2023-06-19 14:20:36 -0700

    added file 5 x2

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -8,12 +8,14 @@
  */
 int main(void)
 {
-	int i = 0;
+	int i = '0';
+	char n = '\n';
 
-	for (i = 0; i <= 10; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
 	putchar (i);
 	}
+	putchar (n);
 	return (0);
 
 
