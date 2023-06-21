7a13814f4eb7dcf2c7e8948cd38a70cc73ff58de
Solomon <swilliamfr@outlook.com>
2023-06-21 04:54:39 -0700

    added file 7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* print_last_digit - print the last digit of a number
+*
+* Description - a function
+*
+* Return: int
+*/
+int print_last_digit(int a)
+{
+	return a % 10;
+}
