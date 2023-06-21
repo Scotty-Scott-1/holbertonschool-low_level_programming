9784c38cb65585cd1dfc9c7117ac727ceeca515b
Solomon <swilliamfr@outlook.com>
2023-06-21 13:53:24 -0700

    corrected file 7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -2,30 +2,20 @@
 /**
 * print_last_digit - print the last digit of a number
 * @a: a number
+*
 * Description - a function
 *
 * Return: int
 */
 int print_last_digit(int a)
-{ 
-	if (a >= 10)
-	{
-	a = a % 10;
-	_putchar (a);
-	_putchar (a);
-	}
-	else if (a <= -10) 
-	{
-	a = a % 10;
-	_putchar(a);
-	_putchar(a);
-	}
-	else
-	{
-	_putchar(a);
-	_putchar(a);
-	}
+{
+	int b;
 
-	return (0);
-
+	b = a % 10;
+	if (b < 0)
+	{
+	b = -b;
+	}
+	_putchar('0' + b);
+	return (b);
 }
