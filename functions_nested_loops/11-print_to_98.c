9232bec8786d3e748139b6a637eefac1139884f9
Solomon <swilliamfr@outlook.com>
2023-06-22 15:09:25 -0700

    added file 11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_to_98 - a function
+* @n: an int
+* Description - prints numbers
+*
+* Return: void
+*/
+void print_to_98(int n)
+{
+	int re;
+
+
+	if (n <= 98) 
+	{
+	for (re = n; re <= 98; re++) 
+	{
+	printf("%d", re);
+	if (re <= 98) {
+	printf(", ");
+	}
+	}
+	} 
+	else 
+	{
+	for (re = n; re >= 98; re--) 
+	{
+	printf("%d", re);
+	if (re>= 98) 
+	{
+	printf(", ");
+	}
+	}
+	}
+	printf("\n");
+}
