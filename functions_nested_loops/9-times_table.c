ac8a371b6e707e28749074a78684dda5fbd6c30c
Solomon <swilliamfr@outlook.com>
2023-06-22 11:46:34 -0700

    added file 9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+* times_table - print the x9 table with , and space
+*
+* Description - a function
+*
+* Return: void
+*/
+void times_table(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = 0; a <= 9; a++)
+	{
+	for (b = 0; b <= 9; b++)
+	{
+	c = a * b;
+	if (c <= 9)
+	{
+	_putchar('0' + c);
+	}
+	else if (c > 9)
+	{
+	_putchar('0' + c / 10);
+	_putchar('0' + c % 10);
+	}
+	if (b < 9)
+	{
+	_putchar(',');
+	_putchar(' ');
+	}
+	else
+	{
+	_putchar('\n');
+	}
+	}
+	}
+}
