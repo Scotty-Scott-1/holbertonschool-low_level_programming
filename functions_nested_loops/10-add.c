85c13877f0695bd8889f595724f75f0880d6ab15
Solomon <swilliamfr@outlook.com>
2023-06-22 14:30:27 -0700

    added file 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* add - adds ints
+* @a: an int
+* @b: an int
+* Descrption - adds ints
+*
+* Return: c
+*
+*/
+int add(int a, int b)
+{
+	int c = a+b;
+	
+return (c);
+
+
+
+
+
+
+
+
+
+
+
+}
