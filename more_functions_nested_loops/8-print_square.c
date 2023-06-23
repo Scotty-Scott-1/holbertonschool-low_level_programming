7be9246143235c7cb8e8dd19bdafb0b3b185a45b
Solomon <swilliamfr@outlook.com>
2023-06-23 11:36:57 -0700

    added file 8

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 * print_square - print a square
-* @size: an int 
+* @size: an int
 * Description - a function
 *
 * Return: void
@@ -9,27 +9,18 @@
 */
 void print_square(int size)
 {
+	int x;
+	int y;
 
-
-    int x;
-  int y;
-          for (x = 0; x < size; x++)
-          {
-                  for (y = 0; y <= size; y++)
-                  {
-			  if (size > 0){
-                  _putchar('#');}
-                  }
-          }
-          _putchar('\n');
-
-
-
-
-
-
-
-
-
-
+	for (x = 0; x < size; x++)
+	{
+	for (y = 1; y <= size; y++)
+	{
+	if (size > 0)
+	{
+	_putchar('#');
+	}
+	}
+	_putchar('\n');
+	}
 }
