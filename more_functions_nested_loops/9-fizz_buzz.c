a44b1cb29870ed3f2812789767407fcc08d609a2
Solomon <swilliamfr@outlook.com>
2023-06-23 12:53:58 -0700

    added file 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
-* main - fizzbuzz . 3 and 5 FizzBuzz
+* main -  3 and 5 FizzBuzz
 *
-* Description: print 1 to 100. with space.
+* Description - print 1 to 100. with space.
 *
 * Return: 0
 *
@@ -15,24 +15,27 @@ int main(void)
 	{
 	if (i % 3 == 0 && i % 5 == 0)
 	{
-	printf("FizzBuzz ");
+	printf ("FizzBuzz ");
 	}
 	else if (i % 3 == 0 && i % 5 != 0)
-	{printf("Fizz ");
+	{
+	printf ("Fizz ");
 	}
 	else if (i % 5 == 0 && i % 3 != 0 && i <100)
 	{
-	printf("Buzz ");
+	printf ("Buzz ");
 	}
 	else if (i % 5 == 0 && i % 3 != 0 && i ==100)
-	{printf("Buzz");}
+	{
+	printf("Buzz");
+	}
 	else if (i % 3 != 0 && i % 5 !=0)
 	{
-	printf("%d", i);
-	printf(" ");
+	printf ("%d", i);
+	printf (" ");
 	}
 	}
-	printf("\n");
+	printf ("\n");
 
 return (0);
 }
