f65918c38a5d04844d91d20232ddd826f99ccab9
Solomon <swilliamfr@outlook.com>
2023-06-23 07:03:25 -0700

    added file 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_numbers -  prints the numbers, from 0 to 9, followed by a new line
+*
+* Description -  a function
+*
+* Return: void
+*
+*/
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+	_putchar('0' + a);
+	}
+	_putchar('\n');
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#include <stdio.h>
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
+int _putchar(char c);
