72da0b9b971a674368bc6c8ec5aae986488cc76f
Solomon <swilliamfr@outlook.com>
2023-06-23 07:59:06 -0700

    added file 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* more_numbers - print 0 to 14 new line 10 times
+*
+* Description: a function
+*
+* Return: void
+*/
+void more_numbers(void)
+{
+	int a;
+	int b; 
+
+	for (b= 0; b <= 9; b++)
+	{
+	for (a = 0; a <= 14; a++)
+	{
+	if(a >= 10)
+	{
+	_putchar('0' + a / 10);
+	}
+	_putchar('0' + a % 10);
+	}
+	_putchar('\n');
+	}
+}
