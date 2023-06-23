c113a6bd8e0a73a8a6a84450cd14816193fefa61
Solomon <swilliamfr@outlook.com>
2023-06-23 08:56:38 -0700

    added file 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* print_line - print a line
+*
+* Description - Where n is the number of times
+*
+*
+* Return: void
+*
+*
+*/
+void print_line(int n)
+{
+	int a;
+
+	for (a = 0; a <= n; a++)
+	{
+		if (n > 0)
+		{
+		_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
