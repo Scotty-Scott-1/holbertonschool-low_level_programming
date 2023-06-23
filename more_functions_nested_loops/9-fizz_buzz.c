a9f0e5ca5b8441c756033b614149803950c0a920
Solomon <swilliamfr@outlook.com>
2023-06-23 12:43:22 -0700

    added file 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -2,9 +2,9 @@
 /**
 * main - fizzbuzz . 3 and 5 FizzBuzz
 *
-* Description: print 1 to 100. with space. multiples of 3 Fizz 5 Buzz
+* Description: print 1 to 100. with space.
 *
-* Return: 0 
+* Return: 0
 *
 */
 int main(void)
@@ -12,35 +12,27 @@ int main(void)
 	int i;
 
 	for (i = 1; i <= 100; i++)
-	{	if (i % 3 == 0 && i % 5 == 0)	
-		{printf("FizzBuzz "); }
-		else if (i % 3 == 0 && i % 5 != 0)
-		{printf("Fizz ");}
-		else if (i % 5 == 0 && i % 3 != 0 && i <100)
-		{printf("Buzz ");}
-		else if (i % 5 == 0 && i % 3 != 0 && i ==100)
-		{printf("Buzz");}
-		else if (i % 3 != 0 && i % 5 !=0)
-		{
-		printf("%d", i);
-		printf(" ");
-		}
-
+	{
+	if (i % 3 == 0 && i % 5 == 0)
+	{
+	printf("FizzBuzz ");
+	}
+	else if (i % 3 == 0 && i % 5 != 0)
+	{printf("Fizz ");
+	}
+	else if (i % 5 == 0 && i % 3 != 0 && i <100)
+	{
+	printf("Buzz ");
+	}
+	else if (i % 5 == 0 && i % 3 != 0 && i ==100)
+	{printf("Buzz");}
+	else if (i % 3 != 0 && i % 5 !=0)
+	{
+	printf("%d", i);
+	printf(" ");
+	}
 	}
 	printf("\n");
 
-
-
-
-
-
-
-
-
-
-
-
-
 return (0);
-
 }
