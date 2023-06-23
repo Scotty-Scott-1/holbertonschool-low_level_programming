cbcf6551686912a61575a95a1e0b1f4f956392d6
Solomon <swilliamfr@outlook.com>
2023-06-23 04:41:16 -0700

    added file 1

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -9,12 +9,17 @@
 */
 int _isdigit(int c)
 {
-	if (c >= 1 && c <= 9)
-	{
-	return (1);
-	}
-	else
+
+	if (c >= 'a' && c <= 'z')
 	{
 	return (0);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+	return (0);
+	}
+	
+	{
+	return (1);
 	}
 }
