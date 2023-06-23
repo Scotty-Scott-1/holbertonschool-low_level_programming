756f5ec55794d7efa6993298ec5dc5f039abb198
Solomon <swilliamfr@outlook.com>
2023-06-23 14:14:48 -0700

    added file 10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -12,22 +12,20 @@ void print_triangle(int size)
 	int h;
 	int w;
 
-	if (size < 0) 
-	{ _putchar('\n');}
-
-
-for(h = 1; h < size; h++)	
-	
-{
-	for(w = 1; w <= size - h; w++)
-	{_putchar(' ');}
-	
+	if (size < 0)
+	{
+	_putchar('\n');
+	}
+	for (h = 1; h <= size; h++)
+	{
+	for (w = 1; w <= size - h; w++)
+	{
+	_putchar(' ');
+	}
 	for (w = 1; w <= h; w++)
-	{_putchar('#');}
-
-
-	
-	
-_putchar('\n');
-}
+	{
+	_putchar('#');
+	}
+	_putchar('\n');
+	}
 }
