a8bab34832fc9da9f5ed80d358612f30d26be28d
Solomon <swilliamfr@outlook.com>
2023-06-23 11:41:54 -0700

    added file 8

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -12,6 +12,12 @@ void print_square(int size)
 	int x;
 	int y;
 
+	if (size < 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
 	for (x = 0; x < size; x++)
 	{
 	for (y = 1; y <= size; y++)
@@ -22,5 +28,6 @@ void print_square(int size)
 	}
 	}
 	_putchar('\n');
+	}
 	}
 }
