d31a43368c455b3789d1e39ef719eba0246ae2ac
Solomon <swilliamfr@outlook.com>
2023-06-23 11:20:27 -0700

    updated header file

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+* print_square - print a square
+* @size: an int 
+* Description - a function
+*
+* Return: void
+*
+*/
+void print_square(int size)
+{
+
+
+    int x;
+  int y;
+          for (x = 0; x < size; x++)
+          {
+                  for (y = 0; y <= size; y++)
+                  {
+			  if (size > 0){
+                  _putchar('#');}
+                  }
+          }
+          _putchar('\n');
+
+
+
+
+
+
+
+
+
+
+}
