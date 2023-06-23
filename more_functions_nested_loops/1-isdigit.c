b1aab09c2fc70a7b23fe9adb0ac12dd33fc00ca5
Solomon <swilliamfr@outlook.com>
2023-06-23 00:30:13 -0700

    added file 1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* _isdigit - a function
+* @c: a int
+*
+* Description - a function
+*
+* Return: int
+*/
+int _isdigit(int c)
+{
+	if (c >= 1 && c <= 9)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
