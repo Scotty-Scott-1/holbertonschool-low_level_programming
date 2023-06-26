426d6163f5e34c5344168835fb17d05b930ffd12
Solomon <swilliamfr@outlook.com>
2023-06-26 03:08:57 -0700

    added file 0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* reset_to_98 - use a pointer to change a value
+* @ *n: a pointer
+* Description - change vaule of int in source file
+*
+* Return: void
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,6 @@
+Project 1 pointers, strings, arrays
+Admin:
+Directory [done]
+Header file [done]
+README [done]
+
