d577bb62a17fc0b58f6dd56ab130348aa8806351
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-06-27 10:25:18 +0200

    added file 6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* rev_string - print every other char staring by 0
+* @str: a string
+* Description - a function
+*
+* Return: void
+*/
+void puts2(char *str)
+{
+
+int i = 0;
+while (str[i] != '\n')
+{
+_putchar(str['0'+i]);
+i++;
+i++;
+
+}
+
+
+}
