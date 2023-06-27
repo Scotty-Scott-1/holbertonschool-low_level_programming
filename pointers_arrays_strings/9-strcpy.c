1b3437732d317e8470650f54e2429eb3b0430988
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-06-27 16:33:08 +0200

    added file 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _strcpy - copy string pointed to by src
+* @src: an int
+* @dest: an int
+* Description - to pointer dest. include null value
+*
+* Return: char
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+
+
+while (*src != '\0')
+
+{
+*dest = *src;
+src++;
+dest++;
+}
+*dest = '\0';
+
+return (dest);
+}
