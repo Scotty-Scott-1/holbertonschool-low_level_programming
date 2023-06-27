45e39457b1cbaff8c41dbd3b4c85108447e5a106
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-06-27 12:44:21 +0200

    filee 7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -8,36 +8,32 @@
 */
 void puts_half(char *str)
 {
-	int x =0;
+	int x = 0;
 	int half = 0;
 
 	while (str[x] != '\0')
-		{
-			x++;
-		}
+	{
+	x++;
+	}
 
 
-	if (x % 2 == 0 )
+	if (x % 2 == 0)
 	{
 	half = x / 2;
-	while(x > half)
-	{
+	while (x > half)
+		{
 		_putchar(str[half]);
 		half++;
+		}
 	}
-	}
-
 	else
 	{
-	half = x - 1/ 2;
-	while(x > half)
-	{
-	_putchar(str[half]);
-	half++;
+		half = (x - 1) / 2;
+			while (x > half)
+			{
+				_putchar(str[half]);
+				half++;
+			}
 	}
-	}
-
-
 _putchar('\n');
-
 }
