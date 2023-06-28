a8437c6b8152ec53f4d39ebcacd0914d18c64424
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-06-28 21:26:30 +0200

    file 2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -13,17 +13,17 @@ char *_strncpy(char *dest, char *src, int n)
 
 	int i = 0;
 
-	while (i <= n && src[i] != '\0')
+	while (i < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
-	}
 
+	}
 	while (i <= n)
 	{
 
-		i++;
 		dest[i] = '\0';
+		i++;
 	}
 
 	return (dest);
