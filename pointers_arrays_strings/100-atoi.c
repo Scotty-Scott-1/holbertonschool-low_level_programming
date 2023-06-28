8da7be90b306fd4202c1a5b594936dd04a67ce8a
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-06-28 11:53:00 +0200

    file 10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,20 +1,25 @@
+/**
+* _atoi - print atoi
+*
+* @s: a string
+* Description: a function
+*
+* Return: int
+*/
 int _atoi(char *s)
 {
-int sum =0;
-int i = 0;
-int sign = 1;
+	int sum = 0;
+	int i = 0;
 
+	while (s[i] != '\0')
+	{
 
-
-while (s[i] != '\0')
-{
-
-	if (s[i] == '-' )
-	{sign = sign * -1;}
+	if (s[i] == '-')
+	{sum - 1; }
 	else if (s[i] != '-')
-	{sum =sum*10+(s[i]-'0');}
-	else if (s[i]<48 ||s[i]>57)
-	{return 0;}
+	{sum = sum * 10 + (s[i] - '0'); }
+	else if (s[i] < 48 || s[i] > 57)
+	{return (0); }
 	i++;
 }
 
