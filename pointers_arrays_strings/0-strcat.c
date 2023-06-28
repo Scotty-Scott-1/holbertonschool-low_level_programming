45811679a373525d21b168cebd128abfb51f99ab
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-06-28 13:56:15 +0200

    file 0

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -8,21 +8,26 @@
 **/
 char *_strcat(char *dest, char *src)
 {
-int srclen = 0;
-int destlen = 0;
-int i;
-while(*dest != '\0')
-{destlen++;
-dest++;}
+	int srclen = 0;
+	int destlen = 0;
+	int i;
 
-while (*src != '\0')
-{srclen++;
-src++;}
+	while (dest[destlen] != '\0')
+	{
+		destlen++;
+	}
 
+	while (src[srclen] != '\0')
+	{
+		srclen++;
+	}
 
-for(i = 0; i < srclen && *src != '\0';i++)
-{dest[i + destlen] = src[i];}
+	for (i = 0; i <= srclen && src[i] != '\0'; i++)
+	{
+		dest[i + destlen] = src[i];
+	}
 
-dest[i + destlen] = '\0';
-return (dest);
+	dest[i + destlen] = '\0';
+
+	return (dest);
 }
