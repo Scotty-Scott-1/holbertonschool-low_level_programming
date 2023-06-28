1572295835de3846b23ae76e9b9bd439b3acb708
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-06-28 19:57:24 +0200

    added brtty

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,14 +1,23 @@
+/**
+* _strncat - concat str 1 to str nat most n times
+* @dest: a char
+* @src: a char
+* @n: an int
+*
+* Description: a function
+*
+* Return: char
+*
+*/
 char *_strncat(char *dest, char *src, int n)
 {
 	char *destcopy = 0;
 
-	destcopy= dest;
-
+	destcopy = dest;
 	while (*dest != '\0')
 	{
 		dest++;
 	}
-
 	while (*src != '\0' && n > 0)
 	{
 		*dest = *src;
