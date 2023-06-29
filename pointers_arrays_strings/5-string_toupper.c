5be7bbb03162f742512350fb1e357bd1563e48a3
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-06-29 18:08:27 +0200

    added betty style

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -2,7 +2,7 @@
 /**
 * string_toupper - convert to uppercase
 * @str: a string
-* @ptr: a pointer
+*
 * Description: a function
 *
 * Return: char
@@ -11,10 +11,11 @@ char *string_toupper(char *str)
 {
 
 	int i;
-	for (i=0 ; str[i] != '\0'; i++)
-{
-	if(str[i] >= 'a' && str[i] <= 'z')
-	str[i] = str[i] - 32;
-}
-return (str);
+
+		for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		str[i] = str[i] - 32;
+	}
+		return (str);
 }
