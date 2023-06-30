0f1f91387d65f81d78ea863be9af90825e7a9da1
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-06-30 13:25:33 +0200

    added betty

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
 * _strchr - return a pointer to c
 * @c: a char
@@ -13,21 +12,20 @@
 */
 char *_strchr(char *s, char c)
 {
-
 	int i = 0;
 
-
-while (s[i] != '\0')
+	while (s[i] != '\0')
 {
 	if (s[i] == c)
 	{
 	return (&s[i]);
 	}
 	i++;
-
 }
-if (c=='\0')
-{return &s[i];}
+	if (c == '\0')
+{
+	return (&s[i]);
+}
 
 return (0);
 }
