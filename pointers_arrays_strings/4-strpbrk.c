464dacec325685e6d75817833d0b6ca00b31c6f5
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-06-30 17:18:30 +0200

    file 4

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ *_strpbrk - search for first occurances of accept in s
+ * @s: pointer to a string
+ * @accept: pointer to a string
+ * Description: recreate strpbrk functoin
+ *
+ * Return: char
+ *
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0;
+	int j = 0;
+	char *result;
+
+while (s[i] != '\0')
+{
+	j = 0;
+
+	while (accept[j] != '\0')
+	{
+		if (accept[j] == s[i])
+		{
+			result = &s[i];
+			return (result);
+		}
+		j++;
+	}
+i++;
+}
+result = '\0';
+return (result);
+}
