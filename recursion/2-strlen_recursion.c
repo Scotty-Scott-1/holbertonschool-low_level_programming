0015a42d182adff2068bb49ac9bbc74d93dcabe5
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-07-03 15:30:19 +0200

    added file 2

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* _strlen_recursion - if not null, inc s, re s+1
+* @s: an int
+* Descrion - else return 0
+*
+* Return: int
+*
+*/
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		s++;
+		_strlen_recursion(s);
+		return (_strlen_recursion(s)+1);
+	}
+		else
+	{
+		return (0);
+	}
+}
