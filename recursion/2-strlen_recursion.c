eb64547562896e0e90c7a283d56c1f1a3bf343e3
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-07-03 15:44:00 +0200

    corrected file 2

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -9,14 +9,13 @@
 */
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-		return (0);
+		s++;
+		return (_strlen_recursion(s) + 1);
 	}
 		else
 	{
-		s++;
-		_strlen_recursion(s);
-		return (_strlen_recursion(s)+1);
+		return (0);
 	}
 }
