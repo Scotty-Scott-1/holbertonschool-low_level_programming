885075b84b9a478c88e7aab0aad14515c418cfc9
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-07-03 12:42:02 +0200

    added file 0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+* _puts_recursion - print string using recursion
+* @s: a string
+*
+* Description: print first element. inc. new line
+*
+* Return: void
+*/
+void _puts_recursion(char *s)
+{
+		if ( *s != '\0')
+	{
+			_putchar(*s);
+			s++;
+			_puts_recursion(s);
+		}
+			else
+		{
+			_putchar('\n');
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
