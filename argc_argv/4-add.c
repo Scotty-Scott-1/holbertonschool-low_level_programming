b42fe6fc80df97b1eb53109d49c28c72e8867d99
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-07-06 23:49:22 +0200

    file 4

---
+++ argc_argv/4-add.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+* main - add args if ints
+* @argc: counts args
+* @argv: array of args
+* Decription: or error not int of less than 2
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int result = 0;
+	int i;
+	char *arg;
+	int j;
+	int number;
+
+	if (argc < 3)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		arg = argv[i];
+		for (j = 0; arg[j] != '\0'; j++)
+		{
+			if (!isdigit(*arg))
+			{
+				printf ("Error\n");
+				return(1);
+			}
+		}
+		number = atoi(arg);
+		result = result + number;
+	}
+printf("%d\n", result);
+return (result);
+}
