1b90a8865ed1304fa866733cb264a04cc11a9a7e
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-07-06 11:05:33 +0200

    added file 3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - print sum of 2 args if there are 2
+* @argc: counts args
+* @argv: array of args
+* Decription: or error 
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int result;
+
+	if (argc == 3)
+	{
+		result = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", result);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+return (0);
+}
