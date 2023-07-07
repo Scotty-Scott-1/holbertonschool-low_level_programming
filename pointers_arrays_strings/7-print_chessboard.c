3f17d3b990984230060205fd25391487f15e28a3
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-07-07 14:06:24 +0200

    added betty

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,3 +1,9 @@
+/**
+* print_chessboard - print chars in chess
+* @a: a string
+* Description - a function
+* Return: void
+*/
 #include "main.h"
 #include <stdio.h>
 void print_chessboard(char (*a)[8])
@@ -5,9 +11,9 @@ void print_chessboard(char (*a)[8])
 	int i;
 	int j;
 
-	for (i=0; i != 8 ; i++ )
+	for (i = 0; i != 8; i++)
 	{
-		for(j=0; j != 8;j++)
+		for (j = 0; j != 8; j++)
 		{
 			_putchar(a[i][j]);
 		}
