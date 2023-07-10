00801e03be02d2549ee51450e2e5f5ee9ae42508
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-07-11 00:39:11 +0200

    added file 2 x5

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -17,20 +17,19 @@ char *str_concat(char *s1, char *s2)
 	int x;
 	int y;
 
-	for (i = 0; s1[i] != '\0'; i++)
-	{}
-	for (j = 0; s2[j] != '\0'; j++)
-	{}
-
 	if (s1 == NULL)
 	{
 		s1 = "";
 	}
-
 	if (s2 == NULL)
 	{
 		s2 = "";
 	}
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{}
+	for (j = 0; s2[j] != '\0'; j++)
+	{}
 
 	m = malloc(i + j + 1);
 
