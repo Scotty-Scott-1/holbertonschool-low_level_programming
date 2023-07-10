be6eb5e721587e3fbaf043391e9c9ea5d09fbcf7
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-07-10 13:37:57 +0200

    added file 1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* _strdup - copy a string to allocated memory
+* @str: a pointer to string
+* Description - return pointer to the copied string
+*
+* Return: char
+*
+*
+*/
+char *_strdup(char *str)
+{
+	int i = 0;
+	char *s2;
+	int j = 0;
+
+	while (str[i] <= '\0')
+	{
+		i++;
+	}
+
+	s2 = malloc(i);
+
+	if (*str == '\0')
+	{
+		return ('\0');
+	}
+
+	while (str[j] != '\0')
+	{
+		s2[j] = str[j];
+		j++;
+	}
+
+	return (s2);
+
+
+}
+
