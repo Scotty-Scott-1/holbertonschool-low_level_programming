143d8b93eb85708cc9649588165e96940748b398
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-07-10 13:56:50 +0200

    test

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -11,23 +11,22 @@
 */
 char *_strdup(char *str)
 {
-	int i = 0;
+	unsigned int i = 0;
 	char *s2;
-	int j = 0;
+	unsigned int j = 0;
 
 	while (str[i] != '\0')
 	{
 		i++;
 	}
-
 	s2 = malloc(i+1);
 
-	if (s2 == 0 || s2 == 0)
+	if (s2 == 0 || i == 0)
 	{
 		return ('\0');
 	}
 
-	while (str[j] != '\0')
+	while (j < i)
 	{
 		s2[j] = str[j];
 		j++;
