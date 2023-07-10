b87953b900a7c152f5851f9e9a22ae67b30c3707
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-07-11 00:27:56 +0200

    added file 2 x3

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,11 +1,10 @@
 #include "main.h"
 #include <stdlib.h>
 /**
-*
 * str_concat - concatanate two strings to mem location
 * @s1: a string
 * @s2: a string
-* Description _ a function
+* Description - a function
 *
 * return: char
 *
@@ -19,34 +18,32 @@ char *str_concat(char *s1, char *s2)
 	int y;
 
 	for (i = 0; s1[i] != '\0'; i++)
+	{}
 	for (j = 0; s2[j] != '\0'; j++)
-
-	m = malloc(i + j + 1);
+	{}
 
 	if (s1 == NULL)
 	{
-		return (s2);
+		s1 = "";
 	}
 
 	if (s2 == NULL)
 	{
-		return (s1);
+		s2 = "";
 	}
 
+	m = malloc(i + j + 1);
 
 	for (x = 0; x < i; x++)
 	{
-	m[x] = s1[x];
+		m[x] = s1[x];
 	}
-
-
-		for (y = 0; y < j; y++)
-		{
-			m[x + y] = s2[y];
-		}
-
+	for (y = 0; y < j; y++)
+	{
+		m[x + y] = s2[y];
+	}
 
 	m[i + j] = '\0';
 
-	return(m);
+	return (m);
 }
