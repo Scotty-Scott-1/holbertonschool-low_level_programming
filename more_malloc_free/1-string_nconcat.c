371d938b126fc32441cf4dea4d48a8af1e02691c
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-07-12 11:39:50 +0200

    test

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -2,58 +2,50 @@
 #include <stdlib.h>
 #include <stdio.h>
 /*
+* string_nconccat - concat 2 strings n times
 *
+* Description - a function
 *
+* Return: char
 *
 **/
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i;
-	unsigned int j;
+	unsigned int i, j, x, y;
 	char *ptr;
-	unsigned int x;
-	unsigned int y;
 
 	if (s1 == NULL)
 	{
 		s1 = "";
 	}
-
 	if (s2 == NULL)
 	{
 		s2 = "";
 	}
-
-	for(i = 0; s2[i] != '\0'; i++)
+	for (i = 0; s2[i] != '\0'; i++)
 	{}
-	for(j = 0; s1[j] != '\0'; j++)
+	for (j = 0; s1[j] != '\0'; j++)
 	{}
-
 	if (i > n)
 	{
-
 		ptr = malloc(n + j + 1);
-		if(ptr == NULL)
+		if (ptr == NULL)
 		{
-			return(NULL);
+			return (NULL);
 		}
 		else
 		{
-			for (x = 0; x < j; x++ )
+			for (x = 0; x < j; x++)
 			{
 				ptr[x] = s1[x];
 			}
-			for (y = 0; y < i; y++)
+			for (y = 0; y < n; y++)
 			{
 				ptr[x + y] = s2[y];
 			}
-			ptr[i+j] = '\0';
-			return (ptr);
 		}
 	}
-
-
-	if(i < n)
+	if (i < n)
 	{
 		ptr = malloc(j + i + 1);
 		if (ptr == NULL)
@@ -70,11 +62,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			{
 				ptr[x + y] = s2[y];
 			}
-			ptr [i + j] = '\0';
-			return(ptr);
 		}
-
 	}
-return (0);
-
+ptr [i + j] = '\0';
+return (ptr);
 }
