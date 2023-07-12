fbda0b58e2e701a70c0a56d5ee82c882fd5dfca7
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-07-12 12:00:02 +0200

    test

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -16,55 +16,40 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *ptr;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
+
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
-	for (i = 0; s2[i] != '\0'; i++)
-	{}
-	for (j = 0; s1[j] != '\0'; j++)
-	{}
+
+	for (i = 0; s2[i] != '\0'; i++);
+
+	for (j = 0; s1[j] != '\0'; j++);
+
 	if (i > n)
 	{
 		ptr = malloc(n + j + 1);
 		if (ptr == NULL)
-		{
 			return (NULL);
-		}
-		else
-		{
-			for (x = 0; x < j; x++)
-			{
-				ptr[x] = s1[x];
-			}
-			for (y = 0; y < n; y++)
-			{
-				ptr[x + y] = s2[y];
-			}
-		}
+
+		for (x = 0; x < j; x++)
+			ptr[x] = s1[x];
+
+		for (y = 0; y < n; y++)
+			ptr[x + y] = s2[y];
 	}
+
 	if (i < n)
 	{
 		ptr = malloc(j + i + 1);
 		if (ptr == NULL)
-		{
 			return (NULL);
-		}
-		else
-		{
-			for (x = 0; x < i; x++)
-			{
-				ptr[x] = s1[x];
-			}
-			for (y = 0; y < j; y++)
-			{
-				ptr[x + y] = s2[y];
-			}
-		}
+
+		for (x = 0; s1[x] != '\0'; x++)
+			ptr[x] = s1[x];
+
+		for (y = 0; s2[y] != '\0'; y++)
+			ptr[x + y] = s2[y];
 	}
-ptr [i + j] = '\0';
-return (ptr);
+	ptr [i + j] = '\0';
+	return (ptr);
 }
