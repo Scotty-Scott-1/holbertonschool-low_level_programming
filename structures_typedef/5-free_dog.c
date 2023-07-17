93d63af04a1cd88e912e2e40054e253c0d8241aa
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-07-17 17:29:27 +0200

    added file 5

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,9 +1,17 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+* free_dog - passes args/members from dog
+* @d: dog
+* Description: free members then free struct
+* Return: void
+*/
 void free_dog(dog_t *d)
 {
+	if (d == NULL)
+	return;
+
 	free(d->name);
 	free(d->owner);
 	free(d);
