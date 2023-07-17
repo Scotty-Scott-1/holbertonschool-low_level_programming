37414567b1cfb6928bfa8c4f029b815044b98028
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-07-17 13:52:03 +0200

    corrected file 2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -15,7 +15,24 @@ void print_dog(struct dog *d)
 	{
 		return;
 	}
-printf("Name: %s\n", d->name);
-printf("Age: %f\n", d->age);
-printf("Owner: %s\n", d->owner);
+
+	if (d->name != NULL)
+	{
+		printf("Name: %s\n", d->name);
+	}
+		else
+	{
+		printf("Name: (nil)");
+	}
+
+	printf("Age: %f\n", d->age);
+
+	if (d->owner != NULL)
+	{
+		printf("Owner: %s\n", d->owner);
+	}
+	else
+	{
+		printf("Owner: (nil)");
+	}
 }
