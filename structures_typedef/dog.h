730204925dc6d94f41e71ddfe67e36a82269d3b6
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-07-17 12:04:56 +0200

    added dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+* struct dog - a description of a dog
+* @name: dog's name
+* @age: dog's age
+* @owner: odg's owner
+* Description: a struct with details of a dog
+*
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif /* DOG_H */
+
