e1b1e89e0fea75a991c4a63d6980e0011481d05b
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-07-18 12:17:28 +0200

    updated file 0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "function_pointers.h"
 /**
 * print_name - function that calls f
@@ -12,6 +13,10 @@
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (*f == NULL)
+	{
+		return;
+	}
 
 	f(name);
 }
