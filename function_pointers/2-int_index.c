95a56be5fd19286ed6ba6a89211ae5e0e8f7aaf2
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-07-18 14:33:39 +0200

    test

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -17,14 +17,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		return (-1);
 	}
-	if(*array == 0)
+	if (*array == 0)
 	{
 		return (-1);
 	}
 
 	while (i < size)
 	{
-	
 		if (cmp(array[i]) != 0)
 		{
 			return (i);
