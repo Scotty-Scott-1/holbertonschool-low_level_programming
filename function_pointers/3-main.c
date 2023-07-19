c1d21a354b1979aaad56314587f65e5c93a2d60c
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-07-19 11:44:12 +0200

    main file

---
+++ function_pointers/3-main.c
@@ -0,0 +1,35 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+int (*getoperator) (int,int);
+int number1;
+int number2;
+int result;
+
+if (argc != 4)
+{
+	printf("Error\n %d", argc);
+	exit(98);
+}
+getoperator = get_op_func(argv[2]);
+number1 = atoi(argv[1]);
+number2 = atoi(argv[3]);
+
+if (getoperator == NULL)
+{
+	printf("Error\n");
+	exit(99);
+}
+if ((getoperator == op_mul || getoperator == op_mod) && number2 == 0)
+{
+	printf("Error\n");
+	exit(100);
+}
+result = getoperator(number1, number2);
+printf("%d\n", result);
+return (result);
+
+}
