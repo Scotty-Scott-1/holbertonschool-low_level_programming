e36ea4469f3b76a4f86b744160d6670a067be6b1
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-07-19 11:49:24 +0200

    test

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -12,12 +12,12 @@ int (*get_op_func(char *s))(int, int)
         {"%", op_mod},
         {NULL, NULL}
     };
-    
+
     int i=0;
 
 	while(ops[i].op != NULL)
 	{
-    if (*s == *ops[i].op)
+    if (*s == *ops[i].op && *(s + 1) == '\0')
 		{
     return (ops[i].f);
     }
