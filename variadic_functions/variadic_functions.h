40bcc29c3f1785d06481dfc3ea4854397d092466
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-07-21 14:44:31 +0200

    test sturct

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,7 +1,19 @@
+#include <stdarg.h>
+#include <stddef.h>
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 int sum_them_all(const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+typedef struct refernce
+{
+	char *ref;
+	void (*func)(va_list args);
+} ref_t;
+void fun_char(va_list args);
+void fun_int (va_list args);
+void fun_float (va_list args);
+void fun_string (va_list args);
+
 #endif /* VARIADIC_FUNCTIONS_H */
