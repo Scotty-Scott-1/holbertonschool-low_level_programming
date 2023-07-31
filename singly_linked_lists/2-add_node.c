a2ef0dc88fabc59a07ac7d888415f752b4ad64d1
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-07-31 20:58:39 +0200

    testing file 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -14,19 +14,38 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	int i = 0;
+	int j = 0;
 
 list_t *newNode = malloc(sizeof(list_t));
 if (newNode == NULL)
 {
 	return (NULL);
 }
-while (str[i] != '\0')
-{
-i++;
-}
-newNode->str = strdup((str));
+/*----------------------------------------------------------------------*/
 
-newNode->len = i;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	newNode->str = malloc(i + 1);
+
+	if (newNode->str == NULL)
+	{
+		free(newNode);
+		return (NULL);
+	}
+
+	while (j < i)
+	{
+		newNode->str[j] = str[j];
+		j++;
+	}
+
+	newNode->str[j] = '\0';
+	newNode->len = i;
+/*------------------------------------------------------------------------*/
+
 *head = newNode;
 return (newNode);
 }
