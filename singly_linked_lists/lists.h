a8ee3d16adf1d86b242b67121d6835b4d30c384b
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-07-31 13:18:21 +0200

    corrected double inclusion and new lines

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,9 +1,10 @@
+#ifndef LISTS_H
+#define LISTS_H
+
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 
-#ifndef LISTS_H
-#define LISTS_H
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -14,9 +15,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
