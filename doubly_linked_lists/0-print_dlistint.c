045b325829f6dc961df017c74caaa93a4fe80105
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-08-01 14:51:34 +0200

    corrected file x2

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -14,16 +14,9 @@ size_t print_dlistint(const dlistint_t *h)
 
 	while (h != NULL)
 	{
-
-		if (h->n != '\0')
-		{
-			printf("%d\n", h->n);
-
-		}
-			h = h->next;
-			i++;
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
 	}
-
-
 return (i);
 }
