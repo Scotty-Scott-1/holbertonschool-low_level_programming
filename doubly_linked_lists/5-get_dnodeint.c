e5615f6ad8337795851aa21758590a854e242f78
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-08-02 12:14:15 +0200

    added file 5

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+/**
+* get_dnodeint_at_index - adds a node the at the nth position
+*
+* Description: adds a new node
+* @head: pointer to  head
+* @index: the position that new node is inserted
+* Return: address of new node
+*
+*/
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+dlistint_t *newNode;
+
+	unsigned int i = 0;
+	dlistint_t *temp = head;
+
+
+	newNode = malloc(sizeof(dlistint_t));
+
+
+	if (newNode == NULL || head == NULL)
+	{
+		return (NULL);
+	}
+
+	while (index >= i)
+	{
+		temp = temp->next;
+		i++;
+	}
+	if (head != NULL)
+	{
+	newNode->next = temp->next;
+	newNode->prev = temp->prev;
+	temp = temp->next;
+	temp->prev = newNode;
+	newNode->next = temp;
+	}
+return (newNode);
+}
