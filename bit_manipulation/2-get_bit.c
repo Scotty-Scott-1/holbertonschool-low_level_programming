d942eda90b1110fef79e702bb3c01fa1f31daecd
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-08-04 16:53:56 +0200

    corrected file 2

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -4,15 +4,21 @@
 * get_bit - get the bit at the nth index
 * @n: the int passed
 * @index: index position
-* Description: convert a int to binary
+* Description: get bit at the nth position
 *
-* Return: void
+* Return: specifed bit or -1 if error
 */
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int shift = 1;
 	unsigned int mask;
 	unsigned int result;
+
+	if (index > 8 * 8)
+	{
+		return (-1);
+	}
+
 
 	mask = shift << index;
 	result = (n & mask) != 0;
