35345a615974e3282f43e1330524b2d85e71a866
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-08-04 18:01:51 +0200

    added file 4

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* clear_bit - set the bit at the nth index to 0
+* @n: pointer to an int
+* @index: index position
+* Description: set bit at the nth position to 0
+*
+* Return: 1 for success or -1 for error
+*/
+int clear_bit(unsigned long int *n, unsigned int index)
+
+{
+
+	unsigned long int shift = 1;
+	unsigned long int mask;
+
+	if (index > 8 * 8)
+	{
+		return (-1);
+	}
+
+	mask = ~shift << (index);
+	*n = *n & mask;
+
+
+return (1);
+
+}
