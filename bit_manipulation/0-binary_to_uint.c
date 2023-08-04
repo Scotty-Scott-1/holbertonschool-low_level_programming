52ce2d60838c831739f0b76c75797f50bedb0d58
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-08-04 13:50:13 +0200

    added file 0

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* binary_to_uint - convert a binary to int
+* @b: char string containing binary
+* Description: convert binary to int
+* Return: result
+*/
+unsigned int binary_to_uint(const char *b)
+{
+	int i = 0;
+	unsigned int result = 0;
+	unsigned int powerof = 1;
+
+	while (b[i] != '\0')
+	{
+		if (i > 31)
+		{
+			return (0);
+		}
+		if (b[i] - '0' == 0 || b[i] - '0' == 1)
+		{
+			i++;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	i -= 1;
+	while (i >= 0)
+	{
+		if (b[i] - '0' == 1)
+	{
+		result += powerof;
+	}
+		i--;
+		powerof *= 2;
+	}
+return (result);
+}
