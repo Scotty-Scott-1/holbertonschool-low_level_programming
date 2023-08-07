75582912b5bcccf37ae96a3f09beea44e0ae8510
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-08-07 22:34:50 +0200

    handled null terminator

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -27,7 +27,7 @@ int create_file(const char *filename, char *text_content)
 	if (fileDesc == -1)
 		return (-1);
 
-	buffer = (char *) malloc(sizeof(char) * i + 1);
+	buffer = (char *) malloc(sizeof(char) * i);
 	if (buffer == NULL)
 	{
 		close(fileDesc);
@@ -39,8 +39,8 @@ int create_file(const char *filename, char *text_content)
 		buffer[j] = text_content[j];
 		j++;
 	}
-	buffer[i] = '\0';
-	output = write(fileDesc, buffer, i + 1);
+	/*buffer[i] = '\0';*/
+	output = write(fileDesc, buffer, i);
 	if (output == -1)
 	{
 		close(fileDesc);
