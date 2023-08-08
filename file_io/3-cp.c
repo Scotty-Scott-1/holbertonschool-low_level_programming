7256362b498b3890899d5fb4f4b2add82ccbfa41
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-08-09 00:44:55 +0200

    correction x3 file 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		write(2, "Usage: cp file_from file_to\n", 29);
+		write(2, "Usage: cp file_from file_to\n", 28);
 		exit(97);
 	}
 
@@ -32,7 +32,7 @@ int main(int argc, char *argv[])
 		return (-1);
 	}
 
-	fileDesc_2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 664);
+	fileDesc_2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0644);
 	if(fileDesc_2 == -1)
 	{
 		dprintf(2,"Error: Can't write to %s\n", argv[2]);
