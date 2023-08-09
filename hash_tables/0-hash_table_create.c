e59beadd57b0448bb2556808076b457c9ea50e62
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-08-09 14:08:27 +0200

    testing file 0

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+
+
+#include "hash_tables.h"
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i = 0;
+
+	hash_table_t *hash = malloc(sizeof(hash_table_t));
+	hash->array = calloc(size, sizeof(hash_node_t));
+
+	hash->size = size;
+	if(hash == NULL)
+	{
+		return (NULL);
+	}
+	while(i < hash->size)
+	{
+		hash->array[i] = NULL;
+		i++;
+	}
+
+
+return (hash);
+
+
+
+}
