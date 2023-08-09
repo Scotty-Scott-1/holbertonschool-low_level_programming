567e7ea3341882fb71f996d040d341620ec08afd
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-08-09 14:18:38 +0200

    test 4

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -6,18 +6,15 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i = 0;
 
-	hash_table_t *hash = malloc(sizeof(hash_table_t));
-	hash->array = calloc(size, sizeof(hash_node_t*));
+	hash_table_t *hash;
 
-	hash->size = size;
-
-
+	hash = malloc(sizeof(hash_table_t));
 	if(hash == NULL)
 	{
 		return (NULL);
 	}
-
-
+	hash->size = size;
+	hash->array = calloc(size, sizeof(hash_node_t*));
 
 	while(i < hash->size)
 	{
