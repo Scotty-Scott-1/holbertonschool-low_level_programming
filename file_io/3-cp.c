1095c4e9d0aee47c182dbbe4ce56725ef7740990
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-08-11 14:35:03 +0200

    test x3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
 	fileDesc_2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fileDesc_2 == -1)
 	{
-		dprintf(1, "Error: Can't write to %s\n", argv[2]);
+		dprintf(2, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 	umask(mask);
