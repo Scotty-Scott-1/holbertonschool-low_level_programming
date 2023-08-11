d732268bd8d305d22ecb4679833db12264ee7c12
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-08-11 13:34:50 +0200

    updated file 0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -8,16 +8,29 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i = 0;
-
 	hash_table_t *hash;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
 
 	hash = malloc(sizeof(hash_table_t));
 	if (hash == NULL)
 	{
 		return (NULL);
 	}
+
+
 	hash->size = size;
-	hash->array = calloc(size, sizeof(hash_node_t *));
+
+	hash->array = malloc(sizeof(hash_node_t) * size);
+	if(hash->array == NULL)
+	{
+		return (NULL);
+		free(hash);
+	}
+
 
 	while (i < hash->size)
 	{
