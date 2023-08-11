d2197560fcd166da2531daa87cc4675e8b650b4c
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-08-11 14:11:28 +0200

    corrected free memory

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -24,32 +24,30 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 index = key_index((unsigned char *)key, ht->size);
 
+tempNode = ht->array[index];
+
+while (tempNode != NULL)
+{
+	if (strcmp(tempNode->key, key) == 0)
+	{
+		free(tempNode->value);
+		tempNode->value = strdup(value);
+		return (1);
+	}
+	tempNode = tempNode->next;
+}
 newNode = malloc(sizeof(hash_node_t));
 if (newNode == NULL)
 {
 return (0);
 }
-
 newNode->key = strdup(key);
 newNode->value = strdup(value);
-if(newNode->key == NULL||newNode->value == NULL)
+if (newNode->key == NULL || newNode->value == NULL)
 {
 	free(newNode);
 	return (0);
 }
-
-tempNode = ht->array[index];
-
-while (tempNode != NULL)
-{
-	if (strcmp(tempNode->key, newNode->key) == 0)
-	{
-		tempNode->value = newNode->value;
-		return (1);
-	}
-	tempNode = tempNode->next;
-}
-
 newNode->next = ht->array[index];
 ht->array[index] = newNode;
 return (1);
--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -13,7 +13,7 @@ void hash_table_delete(hash_table_t *ht)
 	hash_node_t *temp;
 	hash_node_t *temp2;
 
-	if(ht == NULL)
+	if (ht == NULL)
 	{
 		return;
 	}
